a5a8cb2c94d0ab2bedf6aaf569c7992537f57a35
franknyarkoh <33531218+franknyarkoh@users.noreply.github.com>
2021-03-29 18:07:27 +0000

    Update room_order.js

--- bookings/room_management/doctype/room_order/room_order.js
+++ bookings/room_management/doctype/room_order/room_order.js
@@ -7,7 +7,7 @@
 //	}
 //});
 
-frappe.ui.form.on("Room Order", "refresh", function(frm, cdt, cdn){
+frappe.ui.form.on("Booking Items", "refresh", function(frm, cdt, cdn){
 var d = locals[cdt][cdn];   
 frappe.model.set_value(cdt, cdn, "booking_nights", frappe.datetime.get_day_diff( d.booking_checkout , d.booking_checkin));
     refresh_field("booking_nights");

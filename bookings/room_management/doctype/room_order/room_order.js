be51a640886444053b58ac8de1d6e2c419273b61
franknyarkoh <33531218+franknyarkoh@users.noreply.github.com>
2021-03-29 17:44:39 +0000

    Update room_order.js

--- bookings/room_management/doctype/room_order/room_order.js
+++ bookings/room_management/doctype/room_order/room_order.js
@@ -1,8 +1,14 @@
 // Copyright (c) 2019, Frank Nyarkoh and contributors
 // For license information, please see license.txt
 
-frappe.ui.form.on('Room Order', {
-	refresh: function(frm) {
+// frappe.ui.form.on('Room Order', {
+//	refresh: function(frm) {
+//
+//	}
+//});
 
-	}
+frappe.ui.form.on("Room Order", "refresh", function(frm, cdt, cdn){
+var d = locals[cdt][cdn];   
+frappe.model.set_value(cdt, cdn, "booking_nights", frappe.datetime.get_day_diff( d.booking_checkout , d.booking_checkin));
+    refresh_field("booking_nights");
 });
